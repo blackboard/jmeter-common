a5447da2634ad77e90d9a5ae9190dacdcd822570
Michael Olague <michael.olague@blackboard.com>
2014-02-19 00:50:29 -0800

    removing underscores to coform with jmeter convention

--- src/main/java/blackboard/jmeter/functions/ReadLine.java
+++ src/main/java/blackboard/jmeter/functions/ReadLine.java
@@ -42,22 +42,22 @@ import com.blackboard.learn.mobile.mlcs.tests.performance.replay.PersistentFileR
 
 public class ReadLine extends AbstractFunction implements TestStateListener 
 {
-	private static final Logger _log = LoggingManager.getLoggerForClass();
+	private static final Logger log = LoggingManager.getLoggerForClass();
 
-	private static final List<String> _desc = new LinkedList<String>();
+	private static final List<String> desc = new LinkedList<String>();
 
 	private static final String KEY = "__ReadLine"; // Function name
 
 
 	static 
 	{
-		_desc.add(JMeterUtils.getResString( "read_file_file_name" ));  //$NON-NLS-1$
-		_desc.add(JMeterUtils.getResString( "function_name_parpot" )); //$NON-NLS-1$
-		_desc.add(JMeterUtils.getResString("use_multiple_instance"));
-		_desc.add(JMeterUtils.getResString( "desired_rate" )); //$NON-NLS-1$
-		_desc.add(JMeterUtils.getResString( "current_rate" )); //$NON-NLS-1$
-		_desc.add(JMeterUtils.getResString( "current_request"));
-		_desc.add(JMeterUtils.getResString( "current_time" ));
+		desc.add(JMeterUtils.getResString( "read_file_file_name" ));  //$NON-NLS-1$
+		desc.add(JMeterUtils.getResString( "function_name_parpot" )); //$NON-NLS-1$
+		desc.add(JMeterUtils.getResString("use_multiple_instance"));
+		desc.add(JMeterUtils.getResString( "desired_rate" )); //$NON-NLS-1$
+		desc.add(JMeterUtils.getResString( "current_rate" )); //$NON-NLS-1$
+		desc.add(JMeterUtils.getResString( "current_request"));
+		desc.add(JMeterUtils.getResString( "current_time" ));
 	}
 
 	private static final int MIN_PARAM_COUNT = 1;
@@ -66,13 +66,13 @@ public class ReadLine extends AbstractFunction implements TestStateListener
 	
 	private static final int MAX_THREAD_COUNT = 10;
 
-	private Object[] _values;
+	private Object[] values;
 
 	private BufferedReader _myBread = null; // BufferedReader
 
-	private boolean _firstTime = true; // Is the file to be opened first?
+	private boolean firstTime = true; // Is the file to be opened first?
 
-	private boolean _multipleUse = false; // Multiple thread use?
+	private boolean multipleUse = false; // Multiple thread use?
 
 	private int currentRate; // current rate of requests/minute
 
@@ -82,17 +82,17 @@ public class ReadLine extends AbstractFunction implements TestStateListener
 	
 	private long currentTime; // Current location in time relative to the beginning of the test
 
-	private String _fileName; // needed for error messages
+	private String fileName; // needed for error messages
 	
 	private static PersistentFileReader globalFileReader = PersistentFileReader.INSTANCE;
 	
-	public static Map<String, BufferedReader> _readers = globalFileReader.getFileReaders();
+	public static Map<String, BufferedReader> readers = globalFileReader.getFileReaders();
 
 	public ReadLine()
 	{ 
-		if ( _log.isDebugEnabled() ) 
+		if ( log.isDebugEnabled() ) 
 		{
-			_log.debug("++++++++ Construct " + this );
+			log.debug("++++++++ Construct " + this );
 		}
 	}
 
@@ -103,62 +103,62 @@ public class ReadLine extends AbstractFunction implements TestStateListener
 			return;
 		}
 		String tn = Thread.currentThread().getName();
-		_log.info(tn + " closing file " + _fileName);
+		log.info(tn + " closing file " + fileName);
 		try
 		{
 			_myBread.close();
 		}
 		catch ( IOException e )
 		{
-			_log.error( "closeFile() error: " + e.toString(), e);//$NON-NLS-1$
+			log.error( "closeFile() error: " + e.toString(), e);//$NON-NLS-1$
 		}
 
 	}
 
-	private synchronized void openFile(boolean _multipleUse) 
+	private synchronized void openFile(boolean multipleUse) 
 	{
 		try
 		{
-			if ( _fileName == null ) 
+			if ( fileName == null ) 
 			{
-				Set<String> fileNames = _readers.keySet();
+				Set<String> fileNames = readers.keySet();
 				if (!fileNames.isEmpty())
 				{
-					String latestFileName = (String) _readers.keySet().toArray()[ _readers.size() ]; // Grab the filename of the latest file opened.
-					_myBread = _readers.get( latestFileName ); // If there is no fileName included, use the latest reader
+					String latestFileName = (String) readers.keySet().toArray()[ readers.size() ]; // Grab the filename of the latest file opened.
+					_myBread = readers.get( latestFileName ); // If there is no fileName included, use the latest reader
 				}
 				else
 				{
-					_log.error( "No file name has been provided and no files have been opened." );
+					log.error( "No file name has been provided and no files have been opened." );
 					throw new IOException("No file name provided and no files open");
 				}
 			}
 			else
 			{
-				if ( _readers.containsKey(_fileName) ) // If the file has already been opened
+				if ( readers.containsKey(fileName) ) // If the file has already been opened
 				{
 					/* Will implement this later. This is an option for the user to select whether or not the same file should be opened independently by all threads
-					if ( _multipleUse )
+					if ( multipleUse )
 					{
-						_myBread = new BufferedReader( new FileReader( _fileName )); // If the latest fileName is included but the file is already open but we allow for multiple readers, then 
+						_myBread = new BufferedReader( new FileReader( fileName )); // If the latest fileName is included but the file is already open but we allow for multiple readers, then 
 					}
 					else
 					{
-						_myBread = _readers.get( _readers.size() ); // Get the latest 
+						_myBread = readers.get( readers.size() ); // Get the latest 
 					}
 					*/
-					_myBread = _readers.get( _fileName ); // set the reader to be the reader of the 
+					_myBread = readers.get( fileName ); // set the reader to be the reader of the 
 				}
 				else
 				{
-					_myBread = new BufferedReader( new FileReader( _fileName ));
+					_myBread = new BufferedReader( new FileReader( fileName ));
 				}
 			}		
 		}
 		catch ( IOException e )
 		{
-			_firstTime = false;
-			_log.error( "openFile() error: " + e.toString(), e );
+			firstTime = false;
+			log.error( "openFile() error: " + e.toString(), e );
 		}
 
 	}
@@ -172,15 +172,15 @@ public class ReadLine extends AbstractFunction implements TestStateListener
 
 		try{
 		
-			if ( _firstTime )
+			if ( firstTime )
 			{
-				openFile( _multipleUse );
-				_firstTime = false;
+				openFile( multipleUse );
+				firstTime = false;
 			}
 			
 			if ( _myBread != null )
 			{	
-				openFile( _multipleUse );
+				openFile( multipleUse );
 				if ( waitTime > 0.0 )
 				{
 					Thread.currentThread().sleep( waitTime );
@@ -211,21 +211,21 @@ public class ReadLine extends AbstractFunction implements TestStateListener
 
 	public long getWaitTime()
 	{
-		long waitTime;
+		long waitTime = 0;
 		
 		return waitTime;
 	}
 	
 	public synchronized void setParameters( Collection<CompoundVariable> parameters ) throws InvalidVariableException 
 	{
-		_log.debug("setParameter - Collection.size=" + parameters.size());
-		_values = parameters.toArray();
+		log.debug("setParameter - Collection.size=" + parameters.size());
+		values = parameters.toArray();
 
-		if ( _log.isDebugEnabled() )
+		if ( log.isDebugEnabled() )
 		{
 			for ( int i = 0; i < parameters.size(); i++ ) 
 			{
-				_log.debug( "i:" + ((CompoundVariable) _values[i]).execute());
+				log.debug( "i:" + ((CompoundVariable) values[i]).execute());
 			}
 
 		}
@@ -244,7 +244,7 @@ public class ReadLine extends AbstractFunction implements TestStateListener
 	@Override
 	public List<String> getArgumentDesc()
 	{
-		return _desc;
+		return desc;
 	}
 
 	/** {@inheritDoc} */
