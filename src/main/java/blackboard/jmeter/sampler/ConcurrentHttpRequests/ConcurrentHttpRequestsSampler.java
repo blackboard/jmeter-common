de0ba72e2695646b079f3bbe28ee4f4dc2784c01
Zhaoxia Yang <Zhaoxia.Yang@blackboard.com>
2014-02-24 16:07:48 +1100

    Make Global Managers, User Defined Variables, pre processors, JMeter Context work with ConcurrentSampler, also added more test scripts for users to refer.

--- src/main/java/blackboard/jmeter/sampler/ConcurrentHttpRequests/ConcurrentHttpRequestsSampler.java
+++ src/main/java/blackboard/jmeter/sampler/ConcurrentHttpRequests/ConcurrentHttpRequestsSampler.java
@@ -7,13 +7,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.jmeter.protocol.http.control.AuthManager;
+import org.apache.jmeter.protocol.http.control.CacheManager;
+import org.apache.jmeter.protocol.http.control.CookieManager;
+import org.apache.jmeter.protocol.http.control.HeaderManager;
+import org.apache.jmeter.protocol.http.sampler.HTTPSampleResult;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerBase;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
-import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.Interruptible;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.testelement.property.JMeterProperty;
+import org.apache.jmeter.testelement.property.NullProperty;
+import org.apache.jmeter.testelement.property.PropertyIterator;
+import org.apache.jmeter.testelement.property.StringProperty;
+import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
@@ -23,11 +33,13 @@ import blackboard.jmeter.sampler.ConcurrentHttpRequests.config.MultipleHttpReque
 import blackboard.jmeter.sampler.ConcurrentHttpRequests.gui.ListContentSplitPanel;
 
 /**
- * The Sampler to Run multiple Http Requests Concurrently
+ * The Sampler to Run multiple Http Requests Concurrently This Sampler won't execute this sample method since it's
+ * extending HTTPSamplerBase with the purpose of reusing the addTestElement method. This helps to make it easy passing
+ * the properties to the sub request samplers.
  * 
  * @author zyang
  */
-public class ConcurrentHttpRequestsSampler extends AbstractSampler implements Interruptible
+public class ConcurrentHttpRequestsSampler extends HTTPSamplerBase implements Interruptible
 {
 
   private static final long serialVersionUID = 2524409451854674747L;
@@ -40,10 +52,14 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
 
   private static final Logger log = LoggingManager.getLoggerForClass();
   ExecutorService _executor;
+  private JMeterContext _jmeterContextOfParentThread;
 
   @Override
   public SampleResult sample( Entry arg0 )
   {
+    // We must get Context here which will be used for the sub requests's variables replacement.
+    // Early setting context to _jmeterContextOfParentThread may miss the variable changes in the preprocessor
+    _jmeterContextOfParentThread = JMeterContextService.getContext();
     MultipleHttpRequestsConfig wholeConfig = (MultipleHttpRequestsConfig) getProperty( ListContentSplitPanel.CONFIG )
         .getObjectValue();
     List<HttpRequestConfig> configs = wholeConfig.getHttpRequestConfigAsList();
@@ -59,11 +75,16 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
       {
         // Use the UrlConfig Element to setup HttpSamplerBase
         String name = config.getName();
-        TestElement element = config.getUrlConfig();
+        TestElement urlConfigElement = config.getUrlConfig();
         HTTPSamplerBase sampler = new HTTPSamplerProxy();
-        sampler.setName( name );
-        sampler.addTestElement( element );
+        // Set sub request sampler with URLConfig
+        sampler.addTestElement( urlConfigElement );
 
+        addTestElementToChildSampler( sampler );
+
+        // Set sub request sampler's name, the name may override the one which passed in from parent Concurrent sampler
+        sampler.setName( name );
+        sampler.setEnabled( true );
         Runnable worker = new HttpRequest( sampler );
         _executor.execute( worker );
       }
@@ -72,7 +93,7 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
       _executor.shutdown();
       // Wait until all threads are finish. 
       // TODO This timeout can be retrieved from the longest timeout of children Http requests.
-      _executor.awaitTermination( 100000, TimeUnit.MILLISECONDS );
+      _executor.awaitTermination( 1000000, TimeUnit.MILLISECONDS );
       // _result.sampleEnd() is unnecessary here because every call of _result.addSubResult( result ) would extend the End Time.
       // Also calling _result.sampleEnd() after _result.addSubResult( result ) would report error "sampleEnd called twice java.lang.Throwable: Invalid call sequence"
       processResult();
@@ -97,6 +118,66 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
   }
 
   /**
+   * Pass the properties from parent sampler to child. There maybe many properties which are useless to the child
+   * sampler but don't hurt much. All the Managers, http Request Defaults value and Variables are the things we want to
+   * pass.
+   * 
+   * @param childSampler
+   */
+  private void addTestElementToChildSampler( HTTPSamplerBase childSampler )
+  {
+    // Set the Http Defaults and User defined variables from Parent Concurrent Sampler to the sub request sampler
+    PropertyIterator iter = this.propertyIterator();
+    while ( iter.hasNext() )
+    {
+      JMeterProperty prop = iter.next();
+      // The managers will be handled separately
+      if ( prop instanceof CookieManager || prop instanceof CookieManager || prop instanceof CacheManager
+           || prop instanceof AuthManager )
+      {
+        continue;
+      }
+      // Here we need to clone, otherwise, the properties will become same between the sampler and parent ConccurentHttpRequestsSampler
+      JMeterProperty samplerProperty = childSampler.getProperty( prop.getName() );
+      if ( samplerProperty instanceof NullProperty
+           || ( samplerProperty instanceof StringProperty && samplerProperty.getStringValue().equals( "" ) ) )
+      {
+        childSampler.setProperty( prop.clone() );
+      }
+      else
+      {
+        samplerProperty.mergeIn( prop.clone() );
+      }
+    }
+
+    // Set the managers from Parent Concurrent Sampler to the sub request sampler
+    CookieManager cookieManager = this.getCookieManager();
+    if ( cookieManager != null )
+    {
+      // CookieManager needs to be cloned to avoid ConcurrentModificationException while one sub request iterating the cookies and other changing the cookie.
+      childSampler.setCookieManager( (CookieManager) cookieManager.clone() );
+    }
+
+    HeaderManager headerManager = this.getHeaderManager();
+    if ( headerManager != null )
+    {
+      childSampler.setHeaderManager( headerManager );
+    }
+
+    CacheManager cacheManager = this.getCacheManager();
+    if ( cacheManager != null )
+    {
+      childSampler.setCacheManager( cacheManager );
+    }
+
+    AuthManager authManager = this.getAuthManager();
+    {
+      childSampler.setAuthManager( authManager );
+    }
+
+  }
+
+  /**
    * set the parent result according the subResults if there is any sub result failed, then the whole sample is
    * considered as "failed"
    */
@@ -116,7 +197,8 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
       }
     }
     int resultOption = this.getPropertyAsInt( Constants.RESULT_OPTION, Constants.ResultOption.ALLPASS.getOptionValue() );
-    if ( ( resultOption == Constants.ResultOption.ALLPASS.getOptionValue() && failureNum > 0 ) || failureNum == subResults.length )
+    if ( ( resultOption == Constants.ResultOption.ALLPASS.getOptionValue() && failureNum > 0 )
+         || failureNum == subResults.length )
     {
       _result.setSuccessful( false );
       _result.setResponseCode( failureResponseCode.toString() );
@@ -138,11 +220,11 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
 
   class HttpRequest implements Runnable
   {
-    HTTPSamplerBase sampler;
+    private final HTTPSamplerBase sampler;
 
-    HttpRequest( HTTPSamplerBase sampler )
+    HttpRequest( HTTPSamplerBase subSampler )
     {
-      this.sampler = sampler;
+      sampler = subSampler;
     }
 
     @Override
@@ -151,6 +233,11 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
       SampleResult result = null;
       try
       {
+        // get context from parent thread, then set to current sub thread
+        JMeterContextService.replaceContext( _jmeterContextOfParentThread );
+        JMeterContext context = JMeterContextService.getContext();
+        // the samplingStarted field may affect the property value: value vs cachedValue
+        context.setSamplingStarted( true );
         result = sampler.sample();
         // synchronized for the addSubResult method, since SampleResult.storeSubResult is not synchronized and may recreate the subResults Array by accident.
         synchronized ( _result )
@@ -172,4 +259,10 @@ public class ConcurrentHttpRequestsSampler extends AbstractSampler implements In
     return null == remains || remains.size() == 0;
   }
 
+  // This sampler won't execute this sample method since it's extending HTTPSamplerBase with the purpose of reusing the addTestElement method.
+  @Override
+  protected HTTPSampleResult sample( java.net.URL u, String method, boolean areFollowingRedirect, int depth )
+  {
+    return null;
+  }
 }
